b816befe9b57034e6533f14a579bdb6b526b6e5e
Narek Ektubaryan <ektubaryannarek1993@gmail.com>
2023-02-22 00:37:07 +0400

    Added form style

--- buttonapp/ContentView.swift
+++ buttonapp/ContentView.swift
@@ -8,41 +8,84 @@
 import SwiftUI
 
 struct ContentView: View {
-    let colors: [Color] = [.orange, .green, .pink]
-    
-    @State var color = Color.pink
-    
-    var body: some View {
-        ZStack {
-            Circle()
-                .frame(width: 200, height: 200)
-                .foregroundColor(color)
-            
-            Button(action: changeColor) {
-                Text("Tap")
-                    .foregroundColor(.black)
-                    .font(.title)
-                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            }
-        }
+
+    enum Form: CaseIterable {
+      case circle, square, triangle
     }
-    
-    func changeColor() {
-        if let index = colors.firstIndex(of: color) {
-            let nextIndex = (index + 1) % colors.count
-            self.color = colors[nextIndex]
-        } else {
-            self.color = colors.first ?? .pink
+
+    let colors: [Color] = [.red, .blue, .orange]
+
+    @State var currentColor = Color.red
+    @State var currentForm:Form = .circle
+
+    var body: some View {
+      HStack {
+        Button {
+          changeColor()
+        } label: {
+          Text("change color")
+            .foregroundColor(.white)
+            .font(.title)
         }
+
+        Button {
+          changeForm()
+        } label: {
+          Text("change form")
+            .foregroundColor(.white)
+            .font(.title)
+        }
+      }
+
+        ZStack {
+          switch currentForm {
+            case .circle:
+              Circle()
+                .fill(currentColor)
+                .frame(width: 200, height: 200)
+            case .square:
+              Rectangle()
+                .fill(currentColor)
+                .frame(width: 200, height: 200)
+            case .triangle:
+              Triangle()
+                .fill(currentColor)
+                .frame(width: 200, height: 200)
+          }
+       }
+    }
+
+    private func changeColor() {
+      if let index = colors.firstIndex(of: currentColor) {
+        let nextIndex = (index + 1) % colors.count
+        self.currentColor = colors[nextIndex]
+      } else {
+        self.currentColor = colors.first ?? .pink
+      }
+    }
+
+    private func changeForm() {
+      if let index = Form.allCases.firstIndex(of: currentForm) {
+        let nextIndex = (index + 1) % Form.allCases.count
+        self.currentForm = Form.allCases[nextIndex]
+      } else {
+        self.currentForm = .triangle
+      }
     }
 }
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
 
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
 
-
-
-
-
+        return path
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
