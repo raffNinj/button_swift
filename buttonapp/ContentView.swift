3d3f66d326a8152226bd5b3f5a479eddbb0f7ecc
Raffi <raffi.astyan@gmail.com>
2023-03-01 15:42:32 +0400

    I add shake button

--- buttonapp/ContentView.swift
+++ buttonapp/ContentView.swift
@@ -10,57 +10,85 @@ import SwiftUI
 struct ContentView: View {
 
     enum Form: CaseIterable {
-      case circle, square, triangle
+      case circle, square, triangle , pentagon
     }
 
-    let colors: [Color] = [.red, .blue, .orange]
+    let colors: [Color] = [.red, .blue, .orange, .black]
 
     @State var currentColor = Color.red
     @State var currentForm:Form = .circle
 
     var body: some View {
-      HStack {
-        Button {
-          changeColor()
-        } label: {
-          Text("change color")
-            .foregroundColor(.white)
-            .font(.title)
-        }
+        VStack{
+            HStack {
+                Button {
+                    changeColor()
+                } label: {
+                    Image("color_changer")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding()
+                        .background(.gray)
+                        .cornerRadius(13)
+                }
+                .offset(x: 59, y: 55)
+                .position(x: -10, y: 10)
+                
+                Button {
+                    changeForm()
+                } label: {
+                    Image("forms_icon")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding()
+                        .background(.gray)
+                        .cornerRadius(13)
+                }
+                .offset(x: 25, y: 55)
+                .position(x: 30, y: 10)
+                Button {
+                    random_color_form()
+                } label: {
+                    Image("shaker")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding()
+                        .background(Color.gray)
+                        .cornerRadius(13)
+                }
+                .offset(x: 80, y: 55)
+                .position(x:50 , y: 10)
+            ZStack {
+              switch currentForm {
+                case .circle:
+                  Circle()
+                    .fill(currentColor)
+                    .frame(width: 200, height: 200)
 
-        Button {
-          changeForm()
-        } label: {
-          Text("change form")
-            .foregroundColor(.white)
-            .font(.title)
+                case .square:
+                  Rectangle()
+                    .fill(currentColor)
+                    .frame(width: 200, height: 200)
+                case .triangle:
+                  Triangle()
+                    .fill(currentColor)
+                    .frame(width: 200, height: 200)
+                case .pentagon:
+                  Pentagon()
+                      .fill(currentColor)
+                      .frame(width: 200,height: 200)
+              }
+           }
+            .offset(x:-117,y:50)
+            .position(x:20,y: 350)
         }
       }
-
-        ZStack {
-          switch currentForm {
-            case .circle:
-              Circle()
-                .fill(currentColor)
-                .frame(width: 200, height: 200)
-            case .square:
-              Rectangle()
-                .fill(currentColor)
-                .frame(width: 200, height: 200)
-            case .triangle:
-              Triangle()
-                .fill(currentColor)
-                .frame(width: 200, height: 200)
-          }
-       }
     }
 
     private func changeColor() {
       if let index = colors.firstIndex(of: currentColor) {
         let nextIndex = (index + 1) % colors.count
         self.currentColor = colors[nextIndex]
-      } else {
-        self.currentColor = colors.first ?? .pink
       }
     }
 
@@ -68,9 +96,15 @@ struct ContentView: View {
       if let index = Form.allCases.firstIndex(of: currentForm) {
         let nextIndex = (index + 1) % Form.allCases.count
         self.currentForm = Form.allCases[nextIndex]
-      } else {
-        self.currentForm = .triangle
       }
+    }
+    private func random_color_form(){
+        let randomColorIndex = Int.random(in: 0..<colors.count)
+            let randomColor = colors[randomColorIndex]
+            let randomFormIndex = Int.random(in: 0..<Form.allCases.count)
+            let randomForm = Form.allCases[randomFormIndex]
+            self.currentColor = randomColor
+            self.currentForm = randomForm
     }
 }
 
@@ -87,6 +121,25 @@ struct Triangle: Shape {
     }
 }
 
+struct Pentagon: Shape {
+    func path(in rect: CGRect) -> Path {
+        let width = rect.width
+        let height = rect.height
+        let pt1 = CGPoint(x: width * 0.5, y: 0)
+        let pt2 = CGPoint(x: width, y: height * 0.4)
+        let pt3 = CGPoint(x: width * 0.8, y: height)
+        let pt4 = CGPoint(x: width * 0.2, y: height)
+        let pt5 = CGPoint(x: 0, y: height * 0.4)
+        var path = Path()
+        path.move(to: pt1)
+        path.addLine(to: pt2)
+        path.addLine(to: pt3)
+        path.addLine(to: pt4)
+        path.addLine(to: pt5)
+        path.closeSubpath()
+        return path
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
